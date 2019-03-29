c2b62b6515d7752d1a33b688c28976072cfb260b
dtak7 <hgbaba@gmail.com>
2019-03-29 16:57:55 -0700

    done with backwards text

---
+++ BackwardsString.java
@@ -0,0 +1,19 @@
+package _00_Text_Funkifier;
+
+public class BackwardsString extends SpecialString {
+
+	public BackwardsString(String s) {
+		super(s);
+	}
+
+	@Override
+	public String funkifyText(String s) {
+		String reverse = "";
+		for(int i = s.length() - 1; i >= 0; i--)
+        {
+            reverse = reverse + s.charAt(i);
+        }
+		return reverse;
+	}
+
+}
